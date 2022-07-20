2ecc735c6893345218e1106e5f20d124dbcc4895
Saad S <saad.shafiq@tintash.com>
2022-07-20 12:35:16 +0500

    tests: adds tests for proposal update function

--- src/voting-contracts/test/proposal.js
+++ src/voting-contracts/test/proposal.js
@@ -1,5 +1,5 @@
 const Proposal = artifacts.require("Proposal");
-const { BN, expectRevert } = require("@openzeppelin/test-helpers");
+const { BN, expectRevert, time, snapshot } = require("@openzeppelin/test-helpers");
 
 contract("Proposal", function (accounts) {
 
@@ -7,8 +7,8 @@ contract("Proposal", function (accounts) {
   const title = "Proposal";
   const uri = "https://sample.com/file1";
   const votingOptions = ['agree', 'disagree', 'abstain'];
-  const startBlock = new BN(10);
-  const endBlock = new BN(20);
+  const startBlock = new BN(100);
+  const endBlock = new BN(200);
 
   describe('owner', () => {
 
@@ -25,7 +25,8 @@ contract("Proposal", function (accounts) {
         votingOptions, 
         startBlock, 
         endBlock, 
-        Proposal.VotingTypes.SingleChoiceVoting)
+        Proposal.VotingTypes.SingleChoiceVoting
+      );
     });
     
     it('should be contract deployer', async () => {
@@ -42,7 +43,48 @@ contract("Proposal", function (accounts) {
         votingOptions, 
         startBlock, 
         endBlock, 
-        Proposal.VotingTypes.SingleChoiceVoting));
+        Proposal.VotingTypes.SingleChoiceVoting
+      ));
     });
   });
+
+  describe('proposal details', () => {
+    // TODO: can be retrieved by calling `getProposalDetails`
+
+    it('can only be updated by proposal owner', async () => {
+      await expectRevert.unspecified(this.proposal.updateProposalDetails(
+        guid, 
+        title, 
+        uri, 
+        votingOptions, 
+        startBlock, 
+        endBlock, 
+        Proposal.VotingTypes.SingleChoiceVoting, 
+        {from: accounts[2]}
+      ));
+    });
+
+    it('can only be updated before start block height is reached', async () => {
+      // get current snapshot of chain 
+      // before moving chain tip forward 
+      const snapshotA = await snapshot();
+      const currentBlock = await web3.eth.getBlockNumber();
+      if (currentBlock < startBlock) {
+        // NOTE: this freezes ganache ui...
+        await time.advanceBlockTo(startBlock);
+      }
+      await expectRevert.unspecified(this.proposal.updateProposalDetails(
+        guid, 
+        title, 
+        uri, 
+        votingOptions, 
+        startBlock, 
+        endBlock, 
+        Proposal.VotingTypes.SingleChoiceVoting
+      ));
+      // restore previous chain state to run further tests
+      await snapshotA.restore();
+    });
+  });
+
 });
